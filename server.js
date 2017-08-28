ca7c50da2e7f293367db11a52df08f8411706137
Andrew Gibson <andrewgib14@gmail.com>
2017-08-28 09:00:09 -0500

    routes working other then post signup form

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express")
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const path = require("path");
+const sessionConfig = require("./sessionConfig.js");
+const user = require("./data.js")
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionConfig));
+
+app.get("/", function (req, res) {
+    console.log(req.session);
+    res.render("home");
+})
+app.get("/home", function (req, res) {
+    res.render("home");
+})
+app.get("/signup", function (req, res) {
+    res.render("signup")
+})
+app.get("/login", function (req, res) {
+    res.render("login");
+})
+
+app.post("/login", function (req, res) {
+    let reqUsername = req.body.username;
+    let reqPassword = req.body.password;
+
+    let foundUser = user.find(function (user) { return user.username === reqUsername })
+    if (!foundUser) {
+        return res.render("login", { errors: ["User not found"] })
+    }
+    if (foundUser.password === reqPassword) {
+        delete foundUser.password;
+        req.session.user = foundUser;
+        res.redirect("/profile");
+    }
+    else {
+        return res.render("login", { errors: ["Password does not match."] });
+    }
+})
+
+app.post("/signup", function (req, res) {
+
+    res.redirect("login");
+})
+app.get("/profile", function (req, res) {
+    res.render("profile", req.session.user);
+})
+
+app.listen(port, function () {
+    console.log(`Server is running on port ${port}`);
+})
---
+++ sessionConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret: 'penguin rose counter strike league of legends red',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 }
+};
